2a4fbefa52a3897537d03f4453cb989563cc4c21
Nanosch Adams <adams@nanosch.de>
2019-07-08 14:04:21 +0200

    add test for custom hex

--- tests/ConsoleHexTemplatesTest.php
+++ tests/ConsoleHexTemplatesTest.php
@@ -3,8 +3,10 @@
 namespace Test;
 
 use Hexopia\Hex\Hex;
+use Hexopia\Hex\Types\HexEmpty;
 use Hexopia\Hex\Types\HexHero;
 use Hexopia\Map\Plotter\Helpers\ConsoleHexTemplates;
+use Tests\Mocks\CustomConsoleHexTemplates;
 
 class ConsoleHexTemplatesTest extends \PHPUnit\Framework\TestCase
 {
@@ -33,5 +35,20 @@ class ConsoleHexTemplatesTest extends \PHPUnit\Framework\TestCase
         $drawing = ConsoleHexTemplates::forHex($hex);
 
         $this->assertEquals('H', $drawing[2][4]);
+    }
+    
+    /**
+     * @test
+     */
+    public function custom_hex()
+    {
+        $hex = new Hex(
+            0, 0,
+            new HexEmpty()
+        );
+
+        $drawing = CustomConsoleHexTemplates::forHex($hex);
+
+        $this->assertEquals('x', $drawing[2][4]);
     }
 }
