0b45d1f71b6c62a4efc65ce88ecc6cc7c4e260d7
Nanosch Adams <adams@nanosch.de>
2019-07-12 07:23:26 +0200

    add example

--- examples/PathBetweenHexMap.php
+++ examples/PathBetweenHexMap.php
@@ -12,50 +12,49 @@ $obstacles = createObstacles();
 
 $map->placeMany($obstacles);
 
-$start = new Hex(0, 1, new HexHighlighted());
+do {
+    $sq = readline('Start Q: ');
+    $sr = readline('Start R: ');
+    $start = new Hex($sq, $sr, new HexHighlighted());
 
-$target = new Hex(-4, 2, new HexHighlighted(33));
+    $needNew = \Hexopia\Hex\Helpers\HexArr::search($start, $obstacles);
+
+    if ($needNew) {
+        echo "point is obstacle". PHP_EOL;
+    }
+
+} while($needNew);
+
+do {
+    $tq = readline('Target Q: ');
+    $tr = readline('Target R: ');
+    $target = new Hex($tq, $tr, new HexHighlighted(33));
+
+    $needNew = \Hexopia\Hex\Helpers\HexArr::search($target, $obstacles) || $target->equals($start);
+
+    if ($needNew) {
+        echo "point is obstacle or start" . PHP_EOL;
+    }
+
+} while($needNew);
 
 $map->placeMany(
     [$start, $target]
 );
 
-$map->pathBetween($start, $target);
+foreach ($map->pathBetween($start, $target) as $step) {
+    $step->type = new HexHighlighted(34);
 
-//$plotter = MapPlotter::draw($map);
-//
-//echo PHP_EOL;
-//
-//$plotter->plot();
-//
-//echo PHP_EOL;
+    $plotter = MapPlotter::draw($map);
+
+    system('clear');
+
+    $plotter->plot();
+
+    sleep(1);
+}
 
 
-//do {
-//    $sq = readline('Start Q: ');
-//    $sr = readline('Start R: ');
-//    $start = new Hex($sq, $sr, new HexHighlighted());
-//
-//    $needNew = \Hexopia\Hex\Helpers\HexArr::search($start, $obstacles);
-//
-//    if ($needNew) {
-//        echo "point is obstacle". PHP_EOL;
-//    }
-//
-//} while($needNew);
-//
-//do {
-//    $tq = readline('Target Q: ');
-//    $tr = readline('Target R: ');
-//    $target = new Hex($tq, $tr, new HexHighlighted(33));
-//
-//    $needNew = \Hexopia\Hex\Helpers\HexArr::search($target, $obstacles) || $target->equals($start);
-//
-//    if ($needNew) {
-//        echo "point is obstacle or start" . PHP_EOL;
-//    }
-//
-//} while($needNew);
 
 
 function createObstacles() {
